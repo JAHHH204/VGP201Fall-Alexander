cec631875566aef9595435e22a0bf343292e1c41
JAHHH204 <alexander.baron@torontofilmschool.ca>
2024-11-30 02:50:54 -0600

    Added Puzzle Mechanics
    added BP_TomeGate
    Added BP_TomeSafe
    Moved Assets to different folder

---
+++ Source/VGP201FPSStarter/BP_TomeSafe.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BP_TomeSafe.h"
+
+// Sets default values
+ABP_TomeSafe::ABP_TomeSafe()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABP_TomeSafe::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABP_TomeSafe::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
